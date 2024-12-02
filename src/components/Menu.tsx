780947195440c8404a004cb51676119a597c60b4
Vimal Raveendran <vimalraveendra@gmail.com>
2024-12-02 01:41:51 +0100

    added NaviagationForm component,updated menu & main Page

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,13 +3,13 @@ import Image from "next/image"
 
 const Menu = () => {
   return (
-    <header className=" w-full max-w-7xl  mr-5 ml-4 mt-6 h-40 bg-gray-50 rounded-lg gap-8 ">
-         <div className="h-full pt-6  pb-5 max-w-lg mx-auto  border-solid border-2 border-indigo-100 flex flex-col items-center justify-between">
+    <header className="  max-w-7xl w-full  mr-5 ml-4 mt-6 h-40 bg-gray-50 rounded-lg gap-8 ">
+         <div className="h-full pt-6  pb-5 max-w-lg mx-auto  flex flex-col items-center justify-between">
              <div className="text-center">
                     <h1 className="text-base font-semibold text-[#101828]">Menu jest puste</h1>
                     <p className="text-sm font-normal text-[#475467]">W tym menu nie ma jeszcze żadnych linków.</p>
              </div>
-             <div className="w-48 h-10 flex bg-red-500 text-center rounded-md justify-center items-center">
+             <div className="w-48 h-10 flex bg-[#7F56D9] text-center rounded-md justify-center items-center">
              <Image
               src="/plus-circle.svg"
               alt="plus"
