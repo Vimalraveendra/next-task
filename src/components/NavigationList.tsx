e44306d5f3679263a190de046d6f3aa2ba3d3981
Vimal Raveendran <vimalraveendra@gmail.com>
2024-12-06 15:50:26 +0100

    added handleDragStart,handleDragEnd,handleDragOver & handleDrop functionalites to properly drag and drop the list items

--- src/components/NavigationList.tsx
+++ src/components/NavigationList.tsx
@@ -18,19 +18,29 @@ export interface INavigationListProps {
     handleToggleForm:()=>void;
     handleAddSubList:(task:ITask)=>void;
     handleUpdateTask:()=>void;
-    handleDeleteTask:(id:number)=>void
+    handleDeleteTask:(id:number|null)=>void;
+    handleDragStart:(task:ITask)=>void;
+    handleDragEnd:()=>void;
+    handleDrop:(task:ITask)=>void
    }
 
 
 const NavigationList:React.FC<INavigationListProps> =
- ({task,newTask, isEdit,editTaskId,listId,handleEditTask,handleCancelTask,handleChangeTask,handleAddTask,handleToggleForm,handleAddSubList,handleUpdateTask,handleDeleteTask}) => {
+ ({task,newTask, isEdit,editTaskId,listId,handleEditTask,handleCancelTask,handleChangeTask,
+  handleAddTask,handleToggleForm,handleAddSubList,handleUpdateTask,handleDeleteTask,
+  handleDragStart,handleDragEnd,handleDrop,
+}) => {
   return (
       <>
                     <li
                         className="flex items-center  justify-between py-4 px-6 border-solid border-b border-[#EAECF0] gap-1  bg-white  flex-wrap  cursor-move rounded-l-md"
                         draggable="true"
+                        onDragStart={() => handleDragStart(task)}
+                        onDragEnd={handleDragEnd}
+                        onDragOver={(e)=>e.preventDefault()}
+                        onDrop={() => handleDrop(task)}
                     >
-                               <div className="flex items-center ">
+                       <div className="flex items-center ">
                                     <div className=" w-10 h-10 py-2 px-2.5 ">
                                                         <Image
                                                             src="/move.svg"
@@ -45,7 +55,7 @@ const NavigationList:React.FC<INavigationListProps> =
                                                             <p className="text-sm font-normal text-[#475467]">{task && task.url}</p>
                                      </div>
                 
-                               </div>
+                              </div>
                                  
                                 <div className="border-solid border border-[#D0D5DD]  rounded-lg shadow-sm gap-2">
                                         <button className="border-solid border-r border-[#D0D5DD] py-2 px-4 text-sm font-semibold text-[#344054]" onClick={()=>handleDeleteTask(task.id)}>Usuń</button>
@@ -86,6 +96,9 @@ const NavigationList:React.FC<INavigationListProps> =
                           handleAddSubList={handleAddSubList}
                           handleUpdateTask={handleUpdateTask}
                           handleDeleteTask={handleDeleteTask}
+                          handleDragStart={handleDragStart}
+                          handleDragEnd={handleDragEnd}
+                          handleDrop={handleDrop}
                            />
                         ))}
        </ul>
